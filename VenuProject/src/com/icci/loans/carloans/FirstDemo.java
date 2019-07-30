a3ecb32e1d45fa581deac8611b40d0e4dcd0f464
venugopal28 <samavenu028@gmail.com>
2019-07-30 11:39:12 +0530

    this is java project

---
+++ VenuProject/src/com/icci/loans/carloans/FirstDemo.java
@@ -0,0 +1,10 @@
+package com.icci.loans.carloans;
+
+public class FirstDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hai");
+	}
+
+}
---
+++ VenuProject/src/com/icci/loans/carloans/SecondDemo.java
@@ -0,0 +1,16 @@
+package com.icci.loans.carloans;
+
+public class SecondDemo {
+
+	public static void main(String[] args) {
+	
+		
+		  int age=20;  
+		    //checking the age  
+		    if(age>18){  
+		        System.out.print("Age is greater than 18");  
+
+	}
+
+}
+}
